e47fd84b8acb9f4ca6730757477bff14cedd9ce6
dongshujin <dongshujin@xiaoqianghome.com>
2016-04-13 16:17:42 +0800

    Change: add Readme

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
 
     public static void main(String[] args) {
+        if (2 == args.length) {
+            System.out.println(args[0]);
+            System.out.println(args[1]);
+        }
         System.out.println("Hello World!");
     }
 }
